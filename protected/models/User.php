7180ca6791f454cd819761d04dd8d5268e76d1fd
p.manikanta swamy <swamy81293@gmail.com>
2016-02-05 15:05:03 +0530

    Generated User model for table_user

---
+++ protected/models/User.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * This is the model class for table "table_user".
+ *
+ * The followings are the available columns in table 'table_user':
+ * @property integer $user_id
+ * @property string $user_email
+ * @property string $user_password
+ * @property string $user_name
+ * @property integer $user_level
+ *
+ * The followings are the available model relations:
+ * @property Booking[] $bookings
+ * @property Listing[] $listings
+ */
+class User extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'table_user';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_email, user_password, user_name, user_level', 'required'),
+			array('user_level', 'numerical', 'integerOnly'=>true),
+			array('user_email', 'length', 'max'=>100),
+			array('user_password', 'length', 'max'=>50),
+			array('user_name', 'length', 'max'=>200),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('user_id, user_email, user_password, user_name, user_level', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'bookings' => array(self::HAS_MANY, 'Booking', 'booking_user_id'),
+			'listings' => array(self::HAS_MANY, 'Listing', 'listing_owner_user_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'user_id' => 'User',
+			'user_email' => 'User Email',
+			'user_password' => 'User Password',
+			'user_name' => 'User Name',
+			'user_level' => 'User Level',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('user_email',$this->user_email,true);
+		$criteria->compare('user_password',$this->user_password,true);
+		$criteria->compare('user_name',$this->user_name,true);
+		$criteria->compare('user_level',$this->user_level);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return User the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
