c05fa4fea1927c4aef541ae08241e040c5019019
p.manikanta swamy <swamy81293@gmail.com>
2016-02-05 15:16:10 +0530

    CRUD generated for User model.

---
+++ protected/views/user/_form.php
@@ -0,0 +1,52 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'user-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'user_email'); ?>
+		<?php echo $form->textField($model,'user_email',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'user_email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'user_password'); ?>
+		<?php echo $form->textField($model,'user_password',array('size'=>50,'maxlength'=>50)); ?>
+		<?php echo $form->error($model,'user_password'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'user_name'); ?>
+		<?php echo $form->textField($model,'user_name',array('size'=>60,'maxlength'=>200)); ?>
+		<?php echo $form->error($model,'user_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'user_level'); ?>
+		<?php echo $form->textField($model,'user_level'); ?>
+		<?php echo $form->error($model,'user_level'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
