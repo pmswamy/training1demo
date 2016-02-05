8f42feaa6f7a06fa3012468d67a7f7750415bfd4
p.manikanta swamy <swamy81293@gmail.com>
2016-02-05 16:15:35 +0530

    Requirement 3 an 4 implemented, 3.   User can view listings. 4.   User can update each listing.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,6 +32,7 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'Register', 'url'=>array('/user/create')),
 				array('label'=>'List a place', 'url'=>array('/listing/create')),
+				array('label'=>'View listings', 'url'=>array('/listing/admin')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
